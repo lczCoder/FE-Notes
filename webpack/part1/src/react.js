ba36fbc2a4a4c10dc758b5d2173cec651902bc43
lczCoder <lczcoder@163.com>
2022-08-22 23:25:31 +0800

    feat:webpack配置

--- webpack/part1/src/react.js
+++ webpack/part1/src/react.js
@@ -2,15 +2,20 @@
 
 import React from "react";
 import ReactDom from "react-dom";
-import './react.less'
+import "./react.less";
+import iconImg from "./assets/find2.png";
 
 class App extends React.Component {
   render() {
-    return <h3 className='title'>组件渲染</h3>;
+    return (
+      <>
+        <div className="box">
+          <h3 className="title">组件渲染</h3>
+          <img src={iconImg} width="50px"></img>
+        </div>
+      </>
+    );
   }
 }
 
-ReactDom.render(
-  <App></App>,
-  document.getElementById("root")
-)
+ReactDom.render(<App></App>, document.getElementById("root"));
--- webpack/part1/webpack.config.js
+++ webpack/part1/webpack.config.js
@@ -1,15 +1,15 @@
 `use strict`;
 
 const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: "development",
-  entry: {
-    main: "./src/index.js",
-    search: "./src/react.js",
-  },
+  mode:'production',
+  entry: "./src/react.js",
   output: {
-    filename: "[name].js",
+    filename: '[name].js',
     path: path.resolve(__dirname, "dist"),
   },
   module: {
@@ -18,14 +18,33 @@ module.exports = {
         test: /\.js$/,
         use: "babel-loader",
       },
+     
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.((c|le)ss)$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',  
+          'less-loader',
+        ],
       },
       {
-        test: /\.less$/,
-        use: ["style-loader", "css-loader", "less-loader"],
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader:'url-loader',
+            options: {
+              limit: 20240,
+            }
+          }
+        ],
       },
     ],
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name]_[contenthash:8].css'
+    })
+  ],
 };
